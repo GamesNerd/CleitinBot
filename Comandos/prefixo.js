e365ce37e3bed08bd48c4be00d9268e0819eb8cb
GamesNerd <71355938+GamesNerd@users.noreply.github.com>
2022-03-19 22:18:38 -0300

    Delete Comandos directory

--- Comandos/prefixo.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-exports.run = (client, message, args)=> {
-   const embed = new Discord.MessageEmbed()
-   .setTitle(`Prefixo!`)
-   .setDescription(`Meu prefixo nesse servidor é (c!) esse prefixo não pode ser alterado por enquanto espero que entenda ;)!`)
-   .setColor("#2f5ffa")
-   .setFooter(`${client.user.tag} - copyright `, client.user.avatarURL());
-   message.channel.send(embed);
-};
--- Comandos/sistemalevel.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-const jimp = require('jimp');
-exports.run = async (client, message, args, ops, database) =>{
-
-	let usuario = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
-
-};
